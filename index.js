0533e58408383a49d75ceb9a2b642f709e176b70
Christoph Nientied <christophnientied@googlemail.com>
2023-04-03 10:19:47 +0200

    defined structure and idea of project

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-Test
+console.log ("hi");
+
+
+// Idee: Satz besteht aus 3 unterschiedlichen Satzbausteinen, der zu randomisierten Sätzen führt:
+
+// 1. Subjekt: I, You, We, They, The Bootcampers, The intructors
+// 2. Verb: slowly walk, swim, crawl, sprint, stumble, watch someone succeeding, bike
+// 3. Objekt: a marathon, 100m, through the Atlantic, to the next bar, only on my Playstation, up the Zugspitze, 
+
